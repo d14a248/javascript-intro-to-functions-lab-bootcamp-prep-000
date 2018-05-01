d456db99d5e8c37b18020af5902f076155204a7c
david landau <thelandaus@gmail.com>
2018-05-01 01:00:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function sayHiToGrandma(string){
   if (string === string.toLowerCase()) {
     return "I can't hear you!";
   } elseif (string === string.toUpperCase()){
-    return
+    return 
   }
 }
